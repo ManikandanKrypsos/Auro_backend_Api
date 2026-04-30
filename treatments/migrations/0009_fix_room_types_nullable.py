e7a67a6bb8d3c17fda1a36f38f0fd1d7e06f7663
harideepak2004 <harideepak.s10@gmail.com>
2026-04-30 11:52:53 +0530

    fix room type nullable via new migration

--- treatments/migrations/0009_fix_room_types_nullable.py
+++ treatments/migrations/0009_fix_room_types_nullable.py
@@ -4,12 +4,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('treatments', '0006_treatment_room_types'),
+        ('treatments', '0008_treatment_rooms_m2m'),
     ]
 
     operations = [
         migrations.RunSQL(
-            sql="ALTER TABLE treatments_treatment ALTER COLUMN room_type DROP NOT NULL;",
+            sql="ALTER TABLE treatments_treatment ALTER COLUMN room_types DROP NOT NULL;",
             reverse_sql="SELECT 1;",
         ),
     ]
