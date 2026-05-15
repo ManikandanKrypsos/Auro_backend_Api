96a3a737753f2770d9da6650e211ddc1ebda66fd
harideepak2004 <harideepak.s10@gmail.com>
2026-05-15 19:50:08 +0530

    fix IsAuthenticated import error

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from users.permissions import IsAdmin
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer, StaffUpdateSerializer
