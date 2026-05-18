a64d8b1b6cbaf79435be598c1cf4f6dd9408ef0e
harideepak2004 <harideepak.s10@gmail.com>
2026-05-18 11:00:50 +0530

    fix staff_id column - rename or add if missing

--- appointments/migrations/0007_rename_therapist_to_staff.py
+++ appointments/migrations/0007_rename_therapist_to_staff.py
@@ -1,10 +1,13 @@
-from django.db import migrations
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('appointments', '0006_add_insession_status'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -12,7 +15,7 @@ class Migration(migrations.Migration):
             sql="""
                 DO $$
                 BEGIN
-                    -- Rename therapist_id to staff_id if it exists
+                    -- If therapist_id exists, rename to staff_id
                     IF EXISTS (
                         SELECT 1 FROM information_schema.columns
                         WHERE table_name = 'appointments_appointment'
@@ -20,21 +23,18 @@ class Migration(migrations.Migration):
                     ) THEN
                         ALTER TABLE appointments_appointment
                         RENAME COLUMN therapist_id TO staff_id;
-                    END IF;
-                END $$;
-            """,
-            reverse_sql="""
-                DO $$
-                BEGIN
-                    IF EXISTS (
+
+                    -- If neither exists, add staff_id column
+                    ELSIF NOT EXISTS (
                         SELECT 1 FROM information_schema.columns
                         WHERE table_name = 'appointments_appointment'
                         AND column_name = 'staff_id'
                     ) THEN
                         ALTER TABLE appointments_appointment
-                        RENAME COLUMN staff_id TO therapist_id;
+                        ADD COLUMN staff_id BIGINT REFERENCES users_user(id);
                     END IF;
                 END $$;
             """,
+            reverse_sql="SELECT 1;",
         ),
     ]
